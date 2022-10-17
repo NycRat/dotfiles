f1963360b05bb0c5a6d7ed263ee26d06a1bde28a
Eric Xiao <ericxiao0000@gmail.com>
2022-10-16 19:05:20 -0700

    hah

--- dot_config/nvim/lua/nycrat/plugins/neovide-config.lua
+++ dot_config/nvim/lua/nycrat/plugins/neovide-config.lua
@@ -4,8 +4,6 @@ if vim.g.neovide  then
   vim.g.neovide_remember_window_size = false
   vim.g.neovide_refresh_rate = 120
   vim.g.neovide_cursor_animation_length = 0
-  vim.g.neovide_cursor_trail_length = 5
-  vim.g.neovide_cursor_antialiasing = true
   vim.g.neovide_transparency = 1
   -- vim.g.neovide_profiler = true
   -- vim.g.neovide_cursor_vfx_mode = "railgun"
