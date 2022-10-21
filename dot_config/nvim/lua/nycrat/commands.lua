8aafa6cce2c2853226eb1b29a3337d0145d8075d
Eric Xiao <ericxiao0000@gmail.com>
2022-10-21 14:38:39 -0700

    commands only work for c++ and c

--- dot_config/nvim/lua/nycrat/commands.lua
+++ dot_config/nvim/lua/nycrat/commands.lua
@@ -1,15 +1,21 @@
 vim.api.nvim_create_user_command("Gcc", function()
-  vim.cmd [[!g++ -std=c++17 -o %:p:r.o %]]
+  local filetype = vim.bo.filetype
+  if (filetype == "c" or filetype == "cpp") then
+    vim.cmd [[!g++ -std=c++17 -o %:p:r.o %]]
+  end
 end, {})
 
 vim.api.nvim_create_user_command("Run", function()
-  vim.cmd [[sp]]
-  vim.cmd [[term %:p:r.o]]
-  vim.cmd [[resize 20N]]
-  local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
-  vim.api.nvim_feedkeys(keys, 'n', false)
+  local filetype = vim.bo.filetype
+  if (filetype == "c" or filetype == "cpp") then
+    vim.cmd [[sp]]
+    vim.cmd [[term %:p:r.o]]
+    vim.cmd [[resize 20N]]
+    local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
+    vim.api.nvim_feedkeys(keys, 'n', false)
 
-  -- vim.cmd[[!./%:r.o]]
+    -- vim.cmd[[!./%:r.o]]
+  end
 end, {})
 
 vim.api.nvim_create_user_command("Ha", function()
