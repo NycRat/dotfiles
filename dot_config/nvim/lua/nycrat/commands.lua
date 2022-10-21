e1a5e85b21dd1d64b61ee26fcf30e21347357e5f
Eric Xiao <ericxiao0000@gmail.com>
2022-10-20 18:46:00 -0700

    fix

--- dot_config/nvim/lua/nycrat/commands.lua
+++ dot_config/nvim/lua/nycrat/commands.lua
@@ -4,7 +4,7 @@ end, {})
 
 vim.api.nvim_create_user_command("Run", function()
     vim.cmd [[sp]]
-    vim.cmd [[term ./%:r.o]]
+    vim.cmd [[term %:r.o]]
     vim.cmd [[resize 20N]]
     local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
     vim.api.nvim_feedkeys(keys, 'n', false)
