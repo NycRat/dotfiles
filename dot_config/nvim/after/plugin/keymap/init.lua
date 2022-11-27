552c45fc37b54ad9c77a6e47979d27294c014293
Eric Xiao <ericxiao0000@gmail.com>
2022-11-27 10:32:39 -0800

    refactor code running commands

--- dot_config/nvim/after/plugin/keymap/init.lua
+++ dot_config/nvim/after/plugin/keymap/init.lua
@@ -83,7 +83,7 @@ nnoremap("<leader>w", "<Cmd>w<CR>")
 nnoremap("<leader>q", "<Cmd>q<CR>")
 nnoremap("<leader><C-o>", "<Cmd>!open %<CR><CR>", silent)
 
--- C++ competitive programming
+-- Running Code
 nnoremap("<leader>cb", "<Cmd>Build<CR>", silent)
 nnoremap("<leader>cl", "<Cmd>Run<CR>", silent)
 nnoremap("<leader>cr", "<Cmd>Ha<CR>", silent)
--- dot_config/nvim/lua/nycrat/commands.lua
+++ dot_config/nvim/lua/nycrat/commands.lua
@@ -1,28 +1,40 @@
+local build_commands = {
+  c = "!g++ -std=c++17 -o %:p:r.o %",
+  cpp = "!g++ -std=c++17 -o %:p:r.o %",
+  rust = "cargo build --release",
+  go = "go build -o %:p:r.o %"
+}
+
+local run_commands = {
+  c = "%:p:r.o",
+  cpp = "%:p:r.o",
+  rust = "cargo run --release",
+  go = "%:p:r.o"
+}
+
 vim.api.nvim_create_user_command("Build", function()
   local filetype = vim.bo.filetype
-  if (filetype == "c" or filetype == "cpp") then
-    vim.cmd [[!g++ -std=c++17 -o %:p:r.o %]]
-  elseif (filetype == "rust") then
-    vim.cmd [[!cargo build --release]]
+
+  for file, command in pairs(build_commands) do
+    if (filetype == file) then
+      vim.cmd("!" .. command)
+      break
+    end
   end
 end, {})
 
 vim.api.nvim_create_user_command("Run", function()
   local filetype = vim.bo.filetype
-  if (filetype == "c" or filetype == "cpp") then
-    vim.cmd [[sp]]
-    vim.cmd [[term %:p:r.o]]
-    vim.cmd [[resize 20N]]
-    local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
-    vim.api.nvim_feedkeys(keys, 'n', false)
-    -- vim.cmd[[!./%:r.o]]
-  elseif (filetype == "rust") then
-    vim.cmd [[sp]]
-    -- vim.cmd [[term ./target/release/target]]
-    vim.cmd [[term cargo run --release]]
-    vim.cmd [[resize 20N]]
-    local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
-    vim.api.nvim_feedkeys(keys, 'n', false)
+
+  for file, command in pairs(run_commands) do
+    if (filetype == file) then
+      vim.cmd("sp")
+      vim.cmd("term " .. command)
+      vim.cmd("resize 20N")
+      local keys = vim.api.nvim_replace_termcodes("i", true, false, true)
+      vim.api.nvim_feedkeys(keys, 'n', false)
+      break
+    end
   end
 end, {})
 
@@ -31,6 +43,6 @@ vim.api.nvim_create_user_command("Ha", function()
   vim.cmd [[Run]]
 end, {})
 
-vim.api.nvim_create_user_command("Config", function()
-  vim.cmd [[cd ~/.config/nvim]]
-end, {})
+vim.api.nvim_create_user_command("Config",
+                                 function() vim.cmd [[cd ~/.config/nvim]] end,
+                                 {})
